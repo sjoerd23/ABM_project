9b9858a52c0e2422aab9cc1f10e3aa9a53781fe3
Kasper van Tulder <kvtulder@gmail.com>
2021-01-08 10:48:44 +0100

    Indent changed to tab, added Multigrid

--- agent.py
+++ agent.py
@@ -2,5 +2,6 @@ from mesa import Model, Agent
 
 class Customer(Agent):
 
-  def __init__(self, unique_id, model):
-    super().__init__(unique_id, model)
+    def __init__(self, unique_id, model):
+        self.grid = MultiGrid(self.width, self.height, torus=True)
+        super().__init__(unique_id, model)
--- model.py
+++ model.py
@@ -1,7 +1,14 @@
 from mesa import Model
+from mesa.space import MultiGrid
 
 class OurModel(Model):
 
-  def __init__(self, N_customers):
-  	slef.N_customers = N_customers
-    
+	def __init__(self, N_customers, width, height):
+		self.N_customers = N_customers
+		self.grid = MultiGrid(self.width, self.height, torus=False)
+
+	def customer_leaves(self):
+		return
+
+	def customer_enters(self):
+		return
