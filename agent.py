54d4c0b046098d7d86ae10c5a9649b41c6e82256
Kasper van Tulder <kvtulder@gmail.com>
2021-01-08 11:22:56 +0100

    Added some basic object functions

--- agent.py
+++ agent.py
@@ -3,5 +3,14 @@ from mesa import Model, Agent
 class Customer(Agent):
 
     def __init__(self, unique_id, model):
-        self.grid = MultiGrid(self.width, self.height, torus=True)
         super().__init__(unique_id, model)
+
+    """Moves the agent to a new location on the grid"""
+    def move(self):
+        steps = self.model.grid.get_neighborhood(self.pos, moore=True)
+        step = self.random.choice(steps)
+        self.model.grid.move_agent(self, step)
+
+    def step(self):
+        self.move()
+
--- model.py
+++ model.py
@@ -1,14 +1,28 @@
 from mesa import Model
 from mesa.space import MultiGrid
+import agent
 
 class OurModel(Model):
 
 	def __init__(self, N_customers, width, height):
-		self.N_customers = N_customers
+
+		# init basic properties
+		self.width = width
+		self.height = height
 		self.grid = MultiGrid(self.width, self.height, torus=False)
 
-	def customer_leaves(self):
-		return
+		# Start adding customers
+		self.N_customers = 0
+		for i in range(N_customers):
+			self.new_customer()
 
-	def customer_enters(self):
-		return
+	"""Adds a new agent to a random location on the grid. Returns the created agent"""
+	def new_customer(self, agent_object):
+		self.N_customers += 1
+		x = round(self.model.random.random() * self.width)
+		y = round(self.model.random.random() * self.height)
+
+		new_agent = agent_object(self.N_customers, self)
+		self.grid.place_agent(new_agent, (x, y))
+
+		return new_agent
