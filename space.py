15922bb03f2a15891055ea6e4572ebfd8dd966af
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2021-01-20 20:30:43 +0100

    updated score functions

--- space.py
+++ space.py
@@ -19,9 +19,18 @@ class SuperMarketGrid(MultiGrid):
         self.default_score = default_score
         self.scores = {}
 
-    def set_score(self, pos, score):
+    def _set_score(self, agent, new_pos, score_formula):
         """Assigns a new score value to a grid position. Private function, scores should be handled internally"""
-        self.scores[pos] = score
+        neighbors = self.get_neighbors(
+            new_pos, moore=False, include_center=True, radius=self.avoid_radius
+        )
+        safe_pos = self.get_safe_pos(neighbors, agent, new_pos)
+
+        affected_cells = self.get_neighborhood(new_pos, moore=False, include_center=True, radius=self.avoid_radius)
+        for cell in affected_cells:
+            if cell not in safe_pos:
+                score = score_formula(new_pos, cell)
+                self.scores[cell] = score
 
     def get_score(self, *cells):
         """Returns the score value corresponding to the given position."""
@@ -46,44 +55,12 @@ class SuperMarketGrid(MultiGrid):
         return safe_pos
 
     def _add_agent_score(self, agent, new_pos):
-        neighbors = self.get_neighbors(
-            new_pos, moore=False, include_center=True, radius=self.avoid_radius
-        )
-        safe_pos = self.get_safe_pos(neighbors, agent, new_pos)
-
-        for neighbor in neighbors:
-            if type(neighbor) is Obstacle:
-                delta_pos = (neighbor.pos[0] - new_pos[0], neighbor.pos[1] - new_pos[1])
-                if delta_pos in core.BARRIER_DICT:
-                    delta_pos_list =  core.BARRIER_DICT[delta_pos]
-                    real_pos = list([(new_pos[0] + delta_pos[0], new_pos[1] + delta_pos[1]) for delta_pos in delta_pos_list])
-                    safe_pos += real_pos
-
-        affected_cells = self.get_neighborhood(new_pos, moore=False, include_center=True, radius=self.avoid_radius)
-        for cell in affected_cells:
-            if cell not in safe_pos:
-                score = self.avoid_radius - core.get_distance(new_pos, cell, "manhattan") + self.get_score(cell)
-                self.set_score(cell, score)
+        score_formula = lambda pos, cell: self.avoid_radius - core.get_distance(new_pos, cell, "manhattan") + self.get_score(cell)
+        self._set_score(agent, new_pos, score_formula)
 
     def _remove_agent_score(self, agent, pos):
-        safe_pos = []
-        neighbors = self.get_neighbors(
-            pos, moore=False, include_center=True, radius=self.avoid_radius
-        )
-        for neighbor in neighbors:
-            if type(neighbor) is Obstacle:
-                delta_pos = (neighbor.pos[0] - pos[0], neighbor.pos[1] - pos[1])
-                if delta_pos in core.BARRIER_DICT:
-                    delta_pos_list = core.BARRIER_DICT[delta_pos]
-                    real_pos = list([(pos[0] + delta_pos[0], pos[1] + delta_pos[1]) for delta_pos in delta_pos_list])
-                    safe_pos += real_pos
-
-
-        affected_cells = self.get_neighborhood(pos, moore=False, include_center=True, radius=self.avoid_radius)
-        for cell in affected_cells:
-            if cell not in safe_pos:
-                score = - self.avoid_radius + core.get_distance(pos, cell, "manhattan") + self.get_score(cell)
-                self.set_score(cell, score)
+        score_formula = lambda pos, cell: -self.avoid_radius + core.get_distance(pos, cell, "manhattan") + self.get_score(cell)
+        self._set_score(agent, pos, score_formula)
 
     def place_agent(self, agent: Agent, pos: Coordinate):
         super().place_agent(agent, pos)
