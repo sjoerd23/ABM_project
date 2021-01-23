00c5227614a194357a5f79a4157b2e3acadfe9e3
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2021-01-23 12:07:44 +0100

    sensitivity_analysis.py

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -33,6 +33,28 @@ def loader():
     return dictex
 
 
+def analyze_datas():
+
+    # load data
+    datas = loader()
+
+    # analyze data
+    for key, value in datas.items():
+        data = value.to_numpy().flatten()
+        print(data)
+        stat_data = data[0:]
+        mean = np.mean(stat_data)
+        stddev = (1.96 * np.std(stat_data, ddof=1)) / np.sqrt(n_steps)
+        print("Problematic contacts: {:.2f}+-{:.2f}".format(mean, stddev))
+
+        # plt.figure()
+        # plt.title("Number of problematic contacts {} customers".format(N_customers))
+        # plt.scatter([j for j in range(len(data))], data)
+        # plt.xlabel("Time (steps)")
+        # plt.ylabel("Number of problematic contacts")
+        # plt.ylim(0, max(data)+1)
+        # plt.show()
+
 def main():
 
     # load supermarket floorplan for simulation
@@ -41,15 +63,15 @@ def main():
     height = len(floorplan[0])
 
     problem = {
-    "num_vars": 4,
-    "names": ["N_customers", "vaccination_prop", "len_shoplist", "basic_compliance"],
-    "bounds": [[25, 125], [0, 1], [0, 25], [0, 1]]
+    "num_vars": 5,
+    "names": ["N_customers", "vaccination_prop", "len_shoplist", "basic_compliance", "vision"],
+    "bounds": [[10, 125], [0, 1], [0, 20], [0, 1], [3, 7]]
     }
 
     # Set the repetitions, the amount of steps, and the amount of distinct values per variable
-    replicates = 3
-    distinct_samples = 3
-    n_steps = 50
+    replicates = 15
+    distinct_samples = 20
+    n_steps = 500
 
     # N_customers = 100
     # vaccination_prop = 0.0
@@ -65,45 +87,25 @@ def main():
         if var == "N_customers" or var == "len_shoplist":
             samples = np.linspace(*problem['bounds'][i], num=distinct_samples, dtype=int)
 
+        for sample in samples:
+            for replicate in range(replicates):
+                if var == "N_customers":
+                    model = CovidSupermarketModel(floorplan, width, height, N_customers=sample)
+                # elif var == "len_shoplist":
+                #     model = CovidSupermarketModel(floorplan, width, height, len_shoplist=sample)
+                elif var == "basic_compliance":
+                    model = CovidSupermarketModel(floorplan, width, height, basic_compliance=sample)
 
-        batch = BatchRunner(
-            CovidSupermarketModel,
-            fixed_parameters={"floorplan": floorplan, "width": width, "height": height},
-            model_reporters = {"n_problematic_contacts": lambda m: m.n_problematic_contacts},
-            max_steps=n_steps,
-            iterations=replicates,
-            variable_parameters={var: samples},
-            display_progress=True
-        )
+                model.run_model(n_steps)
 
-        batch.run_all()
-
-        datas[var] = batch.get_model_vars_dataframe()
+                datas["{}_{}_{}".format(var, sample, replicate)] = model.datacollector.get_model_vars_dataframe()
 
     # save data
     saver(datas)
 
     print("Total simulation time: {:.2f}s".format(time.time()-time_start))
 
-    # load data
-    datas = loader()
-
-    # analyze data
-    for i in range(datas):
-        data = datas[i].to_numpy().flatten()
-
-        stat_data = data[0:]
-        mean = np.mean(stat_data)
-        stddev = (1.96 * np.std(stat_data, ddof=1)) / np.sqrt(n_steps)
-        print("Problematic contacts: {:.2f}+-{:.2f}".format(mean, stddev))
-
-        # plt.figure()
-        # plt.title("Number of problematic contacts {} customers".format(N_customers))
-        # plt.scatter([j for j in range(len(data))], data)
-        # plt.xlabel("Time (steps)")
-        # plt.ylabel("Number of problematic contacts")
-        # plt.ylim(0, max(data)+1)
-        # plt.show()
+    analyze_datas()
 
     return
 
