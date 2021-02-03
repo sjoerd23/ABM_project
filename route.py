638ee30fb164e0bbc29551f15317af26c56b606c
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2021-02-03 15:51:51 +0100

    code clean-up

--- route.py
+++ route.py
@@ -3,6 +3,19 @@ import numpy as np
 
 
 class Position:
+	"""Position object used by the A* algorithm. Stores necessary attributes for the position
+
+    Args:
+        pos (x, y): positon of grid cell
+        g_score (int): score shortest path until current
+		f_score (int): score current best guess
+
+    Attributes:
+        pos (x, y): positon of grid cell
+        g_score (int): score shortest path until current
+		f_score (int): score current best guess
+		parent (Position): previous grid cell
+    """
 	def __init__(self, pos, g_score=np.inf, f_score=np.inf):
 		self.pos = pos
 		self.g_score = g_score
@@ -11,7 +24,27 @@ class Position:
 
 
 class Route:
+	"""Manages path of agent
 
+    Args:
+        model: model object this route is part of
+		start (pos (x, y)): start coordinates
+		goal (pos (x, y)): goal coordinates
+		grid: grid of environment
+		forbidden_type (list): what kind of agents are forbidden to step on
+		forbidden_cells (list): cells that are to be avoided
+
+    Attributes:
+        model: model object this route is part of
+		start (pos (x, y)): start coordinates
+		goal (pos (x, y)): goal coordinates
+		grid: grid of environment
+		forbidden_type (list): what kind of agents are forbidden to step on
+		forbidden_cells (list): cells that are to be avoided
+		shortest (list): path between current positon and goal
+		path_length (int): length of path
+
+    """
 	def __init__(self, model, start, goal, grid, forbidden_type=[], forbidden_cells=[]):
 		self.model = model
 		self.start = start
@@ -27,17 +60,32 @@ class Route:
 			self.path_length = None
 
 	def move_agent(self, agent):
-		"""Moves the agent to the next step, updates the list and self.path_lenght"""
+		"""Moves the agent to the next step, updates the list and self.path_length
+
+		Args:
+			agent (Agent): the agent object to move
+
+		"""
 		self.model.grid.move_agent(agent, self.shortest[-1])
 		self.shortest.pop()
 		self.path_length -= 1
 
 	def get_possible_neighborhood(self, pos, forbidden_cells=[]):
-		"""Returns all the possible locations to walk to."""
+		"""Returns all the possible locations to walk to directly next to agent
+
+		Args:
+			pos (x, y): positon of agent on grid
+			forbidden_cells (list): cells that are to be avoided
+
+		Returns:
+			possible (list): possible neighbouring cells to walk to
+
+		"""
 		possible = []
 		candidates = self.grid.get_neighborhood(pos, False, radius=1)
 
 		for candidate in candidates:
+
 			# check if the cell position is in the forbidden location list
 			if candidate in forbidden_cells:
 				continue
@@ -45,6 +93,7 @@ class Route:
 			content_list = self.grid.get_cell_list_contents(candidate)
 			valid = True
 			for content in content_list:
+
 				# check if the agent type is in the forbidden agent type list
 				if type(content) in self.forbidden_type:
 					valid = False
@@ -55,7 +104,15 @@ class Route:
 		return possible
 
 	def check_if_crowded(self, vision, agent_pos=None):
-		"""Checks if there are people in the way during the next x steps"""
+		"""Checks if there are people in the way during the next steps
+
+		Args:
+			vision (int): value of vision parameter (how far agent can look)
+
+		Returns:
+			score (int): score of other agents on the surrounding grid cells
+
+		"""
 		path_length = len(self.shortest)
 
 		if path_length < vision:
@@ -72,15 +129,21 @@ class Route:
 
 			score += self.grid.get_score(cell) - correction
 
-		# score is always 4 because agent own score
 		return score
 
 	def find_shortest(self):
+		"""Find shortest route using manhattan metric """
 		return self.a_star("manhattan")
 
 	def a_star(self, distance_method):
 		"""A* path finding algorithm.
-		Based on pseudocode on Wikipedia: https://en.wikipedia.org/wiki/A*_search_algorithm"""
+		Based on pseudocode on Wikipedia: https://en.wikipedia.org/wiki/A*_search_algorithm
+
+		Args:
+			distance_method (string: ("chebyshev", "manhattan", "euclidean"), optional):
+				distance metric
+
+		"""
 		start_object = Position(self.start, 0, get_distance(self.start, self.goal))
 		explored = {self.start: start_object}
 		unexplored = {self.start: start_object}
@@ -92,6 +155,7 @@ class Route:
 
 			# check if we reached the destination
 			if current.pos == self.goal:
+
 				# found goal, reconstruct the most efficient route
 				queue = []
 				while current.parent:
@@ -105,14 +169,18 @@ class Route:
 
 			# check all the neighbours
 			for neighbour_pos in self.get_possible_neighborhood(current.pos, self.forbidden_cells):
+				# check if already exlored, if so retrieve stored data
 				if neighbour_pos in explored:
 					neighbour = explored[neighbour_pos]
 				else:
+
+					# not explored yet. Create object
 					explored[neighbour_pos] = Position(neighbour_pos)
 					neighbour = explored[neighbour_pos]
 
 				tentative_g_score = current.g_score + get_distance(current.pos, neighbour.pos, distance_method)
 				if tentative_g_score < neighbour.g_score:
+					
 					# better score, save new calculated score
 					neighbour.g_score = tentative_g_score
 					neighbour.f_score = tentative_g_score + get_distance(neighbour.pos, self.goal, distance_method)
